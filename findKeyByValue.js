8b213b7dfcf8e06e845211895aee7ec7fe0d4cec
Abiola Osinupebi <abiolaosinupebicsa@gmail.com>
2021-01-09 19:21:45 +0000

    adding findKey

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+
+const findKeyByValue = function(showName, value) {
+  const keysArray = Object.keys(showName);
+  for (let element of keysArray) {
+    if (showName[element] === value) {
+      return element;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
